40286aa42a57ea24a2677163997cd873717448cc
YouCanCallMeGus <gustavosantanas.2019@gmail.com>
2025-07-29 00:29:15 -0300

    making calls

--- agent-testing.py
+++ agent-testing.py
@@ -5,6 +5,7 @@ import os
 
 load_dotenv()
 VAPI_KEY = os.getenv("VAPI_API_KEY")
+PHONE_ID = os.getenv("PHONEID")
 
 app = Flask(__name__)
 
@@ -16,15 +17,46 @@ def home():
 
 @app.route("/call", methods=["POST"])
 def call():
+    body_content = request.get_json()
     headers = {
         "Authorization": f"Bearer {VAPI_KEY}"
     }
+
     data = {
         "name": "test",
-        "assistant": {},
+        "assistant": {
+            "model": {
+                "provider": "openai",
+                "model": "gpt-4",
+                "messages": [
+                    {
+                        "role": "system",
+                        "content": f"""
+                            Você é um prestador de serviços que quer confirmar os dados de uma pessoa no hotel!
+
+                            Você tem os dados de um cliente e sua missão é confirmar se ele tem ou não vaga registrada no hotel {body_content["hotelName"]}.
+
+                            Nome: {body_content["name"]}
+                            CPF: {body_content["cpf"]}
+                            Código de confirmação: {body_content["bookCode"]}
+                            CheckIn: {body_content["checkIn"]}
+                            checkOut: {body_content["checkOut"]}
+
+                            Ao terminar a verificação você deve apenas agradecer a confirmação com um "Obrigado pela confirmação!" e encerrar a ligação.
+                        """
+                    }
+                ]
+            },
+            "firstMessage": f"Olá, tudo bem? Esse é o hotel {body_content["name"]}?"
+        },
+        "customer": {
+            "number": f"{body_content["hotelPhone"]}",
+            "name": f"{body_content["hotelName"]}"
+        },
+        "phoneNumberId": PHONE_ID
 
     }
-    response = requests.post(url=vapi_api_url, headers=headers, data=data)
+    response = requests.post(url=vapi_api_url, headers=headers, json=data)
     return response.json(), response.status_code
 
 if __name__ == "__main__":
